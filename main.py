5f95dc1647b21e2ba6386cc82b5785411cd62cac
FranckVachon <franv13007>
2018-10-12 08:26:14 -0400

    Working with decent imaging. Normalizing by the sum of column values at the end

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import random
 from PIL import Image
 
 #matrix indicating how probably it is to change from hidden state ki hidden state kj, e.g. aij
-transition_logprobs = np.log2(np.array([[.99,.01],
-                                        [.01,.99]]))
+transition_logprobs = np.log2(np.array([[.5,.5],
+                                        [.5,.5]]))
 transition_probs = np.array([[.95,.05],
                             [.05,.95]])
 
 #our observation sequences, which we will use to try to deduce the likiest hidden state at each step
 #to make things simple, here our sequence corresponds to the ROW number of the signal in the emission matrix
-sequence_observations = np.array([1,1,1,1,0,0,0,0,0])
+sequence_observations = np.array([1,1,1,1,0,1,0,1,0])
 
 #the viterbi matrix - the one we build dynamically dims: [number_of_hidden_state, num_of_observations]
 viterbi_matrix = np.zeros((transition_probs.shape[0], sequence_observations.shape[0]))
@@ -22,8 +22,8 @@ viterbi_matrix = np.zeros((transition_probs.shape[0], sequence_observations.shap
 
 emission_logprobs =np.log2( np.array([[.9,.1],
                                       [.1,.9]]))
-emission_probs =np.array([[.8,.3],
-                        [.2,.7]])
+emission_probs =np.array([[.9,.1],
+                        [.01,.99]])
 
 def dynamic_viterby_logprob(emission_probs, transition_probs, viter, obs):
     """using log prob to manage undeflow problems"""
@@ -41,6 +41,13 @@ def dynamic_viterby_logprob(emission_probs, transition_probs, viter, obs):
         #now we have a [m,1] vector which we need to multiply by our emission probablity matrix.
         # Each element corresonds to one hidden state, and e = [n*m]
         viter[:, idx + 1] =(emission_probs+max_va[:,None])[observation,:]
+
+        #now we have the value - need to tweak them a little so they show nicely on screen
+        viter = np.absolute(viter)
+        for col in range(0,viter.shape[1]):
+            viter[:,col] = viter[:,col]/np.sum(viter[:,col])
+
+
     print(viter)
     return viter
 
@@ -73,7 +80,7 @@ def viterbi(image):
     """
     row_labels = range(image.shape[0])
     col_labels = range(image.shape[1])
-    plt.imshow(np.absolute(image), cmap='gray_r',)
+    plt.imshow(np.absolute(image), cmap='gray',)
     plt.xticks(range(image.shape[1]), col_labels)
     plt.yticks(range(image.shape[0]), row_labels)
     plt.show()
@@ -83,6 +90,7 @@ def viterbi(image):
 
 #img = dynamic_viterby_normalize(emission_probs,transition_probs,viterbi_matrix, sequence_observations)
 #viterbi(img)
+
 
 img_log = dynamic_viterby_logprob(emission_logprobs,transition_logprobs,viterbi_matrix, sequence_observations)
 viterbi(img_log)
